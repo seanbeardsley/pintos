cfc6452b4a84bd41c066247b1a681926072988dd
Sean Beardsley <alreadysean@gmail.com>
2026-03-21 23:14:50 -0700

    Removed redundant functions

--- src/threads/synch.h
+++ src/threads/synch.h
@@ -42,10 +42,6 @@ void cond_wait (struct condition *, struct lock *);
 void cond_signal (struct condition *, struct lock *);
 void cond_broadcast (struct condition *, struct lock *);
 
-void donate_priority(struct thread *t);
-void refresh_priority(void);
-void remove_donations_for_lock(struct lock *lock);
-
 /** Optimization barrier.
 
    The compiler will not reorder operations across an
