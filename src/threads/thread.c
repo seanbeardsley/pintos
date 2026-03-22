6349cd74c27aec0c87cdbac2ae8721aaee1644b6
Sean Beardsley <alreadysean@gmail.com>
2026-03-21 23:54:33 -0700

    Added refresh priority to exit function

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -331,6 +331,7 @@ thread_exit (void)
   /* Remove thread from all threads list, set our status to dying,
      and schedule another process.  That process will destroy us
      when it calls thread_schedule_tail(). */
+  refresh_priority();
   intr_disable ();
   list_remove (&thread_current()->allelem);
   thread_current ()->status = THREAD_DYING;
